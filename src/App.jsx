9a9d00a501b8d134e8cf7cf44142b7b792268c72
nazar1panyushchyk <npanusik@gmail.com>
2025-09-30 20:55:48 +0300

    added button exit and functional to modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css';
-// import Modal from "./components/Modal";
-import LineChart from "./components/LineChart";
+import Modal from "./components/Modal";
+// import LineChart from "./components/LineChart";
 
 function App() {
   return (
     <>
       
-      {/* <Modal /> */}
-      <LineChart />
+      <Modal />
+      {/* <LineChart /> */}
     </>
   )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,35 @@
 import "../components/Modal.css";
 import logo from "../components/img/logo.png";
+import { IoMdExit } from "react-icons/io";
+import { useState } from "react";
 
 export default function Modal() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className="modal-container">
-        <img src={logo} alt="" />
-        <div className="modal-text">
-          <p>
-            Are you sure you want to <span>log out?</span>
-          </p>
+      {!open && (
+        <button className="exit" onClick={() => setOpen(true)}>
+          Exit <IoMdExit style={{ margin: "-3px"}} />
+        </button>
+      )}
+
+      {open && (
+        <div className="modal-container">
+          <img src={logo} alt="" />
+          <div className="modal-text">
+            <p>
+              Are you sure you want to <span>log out?</span>
+            </p>
+          </div>
+          <div className="modal-buttons">
+            <button className="logout">Logout</button>
+            <button className="cancel" onClick={() => setOpen(false)}>
+              Cancel
+            </button>
+          </div>
         </div>
-        <div className="modal-buttons">
-          <button className="logout">Logout</button>
-          <button className="cancel">Cancel</button>
-        </div>
-      </div>
+      )}
     </>
   );
 }
