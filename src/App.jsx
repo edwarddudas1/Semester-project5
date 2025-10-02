b7bbb8bd4fe73db49d7aa24156566daee46078a4
edwarddudas1 <edwarddudas@gmail.com>
2025-10-02 20:31:48 +0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from'./components/header.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignUp from './registration/Sign-up.jsx'
+import Home from './components/Home.jsx';
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home/>} />
           <Route path="/signup" element={<SignUp/>}/>
         </Routes>
       </BrowserRouter>
