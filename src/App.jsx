6bf340dca17a856a5c3371d64c025ecd1a8acc2a
nazar1panyushchyk <npanusik@gmail.com>
2025-10-13 22:11:58 +0300

    add image swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
-import Modal from "./components/Modal";
+// import Modal from "./components/Modal";
 // import LineChart from "./components/LineChart";
+import Nature from "./components/Nature";
 
 function App() {
   return (
     <>
       
-      <Modal />
+      {/* <Modal /> */}
       {/* <LineChart /> */}
+      <Nature />
     </>
   )
 }
